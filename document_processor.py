bbdc445649ffb36e0a159bbe27578828f7433d35
Bruce Nielson <brucenielson1@gmail.com>
2024-10-12 12:29:26 -0600

    Best handling of roman numerals in front of a title in all caps

--- document_processor.py
+++ document_processor.py
@@ -104,9 +104,14 @@ def get_page_number(paragraph: Tag) -> str:
 def enhance_title(text: str) -> str:
     text = text.strip()
     # If all caps but not a roman numeral and not first word before a space of a sentence roman numeral
-    if text.isupper() and not is_roman_numeral(text) and not is_roman_numeral(text.split(' ', 1)[0]):
-        # If all caps, title case
-        text = text.title()
+    if text.isupper() and not is_roman_numeral(text):
+        # If first word before a space is a roman numeral, leave that part as is
+        first_word = text.split(' ', 1)[0]
+        if is_roman_numeral(first_word) and first_word != text:
+            text = first_word + text[len(first_word):].title()
+        else:
+            # If all caps, title case
+            text = text.title()
         # Replace 'S with 's after title casing
         text = text.replace("'S", "'s")
         text = text.replace("’S", "’s")
@@ -114,7 +119,6 @@ def enhance_title(text: str) -> str:
 
 
 def is_roman_numeral(s: str) -> bool:
-    # Correct placement of the case-insensitive flag at the start
     roman_numeral_pattern = r'(?i)^(M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$'
     return bool(re.match(roman_numeral_pattern, s.strip()))
 
