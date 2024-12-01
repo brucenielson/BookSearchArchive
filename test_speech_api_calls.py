6ed63aa3dcfd4e134dbcfb9a0a8403aa153cf94c
Bruce Nielson <brucenielson1@gmail.com>
2024-11-30 17:56:32 -0700

    Final code

--- test_speech_api_calls.py
+++ test_speech_api_calls.py
@@ -1,9 +1,6 @@
 from huggingface_hub import InferenceClient
 import requests
 from pathlib import Path
-import sounddevice as sd
-import numpy as np
-from transformers import AutoConfig
 
 # Load Hugging Face API secret - Put the secret in a text file and read it
 hf_secret = open(r'D:\Documents\Secrets\huggingface_secret.txt', 'r').read().strip()
@@ -43,17 +40,8 @@ def generate_audio(model_id: str, token: str, text: str):
     try:
         print(f"\n>>> Generating audio with model: {model_id}")
         audio_data = client.text_to_speech(text, model=model_id)
-        model_details = get_model_details(model_id, token)  # Fetch model details
-        config = AutoConfig.from_pretrained(model_id)
-        # sampling_rate = model_details.get("sampling_rate", 16000)
-        # if isinstance(audio_data, bytes):
-        #     # Convert the byte data to a numpy array
-        #     audio_array = np.frombuffer(audio_data, dtype=np.float32)
-        #     # Play the audio using sounddevice
-        #     sd.play(audio_array, samplerate=sampling_rate)
-        #     sd.wait()  # Wait until audio is finished playing
-        #     print(f"Audio played for model {model_id}")
-        #     return True
+        # model_details = get_model_details(model_id, token)
+        # config = AutoConfig.from_pretrained(model_id)
         if isinstance(audio_data, bytes):
             file_name = model_id.replace("/", "_")
             audio_file = Path(f"{file_name}_test_sentence.flac")
@@ -103,20 +91,3 @@ if __name__ == "__main__":
     ]
     text = "Hello, welcome to the world of text to speech!"
     try_models(models, text, hf_secret)
-
-
-# https://discuss.huggingface.co/t/undefined-error-on-inference-api-serverless-for-several-hf-text-to-speech-tasks/79230/3
-# https://huggingface.co/playground?modelId=google/gemma-1.1-2b-it
-# Use Spaces
-# https://huggingface.co/spaces/suno/bark
-# Others found here:
-# https://huggingface.co/tasks/text-to-speech
-# https://huggingface.co/docs/transformers/tasks/text-to-speech
-# https://huggingface.co/spaces/mrfakename/E2-F5-TTS (Your own voice!)
-# https://huggingface.co/spaces/bnielson/testspace
-# https://www.gradio.app/guides/quickstart
-# https://huggingface.co/docs/hub/spaces-sdks-gradio
-# https://www.gradio.app/main/guides/the-interface-class
-# https://www.gradio.app/guides/blocks-and-event-listeners
-# https://www.gradio.app/guides/streaming-ai-generated-audio
-# https://github.com/huggingface/parler-tts
