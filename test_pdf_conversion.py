e7972344546f9d775fe6abbabb39c2d1927a2fe9
Bruce Nielson <brucenielson1@gmail.com>
2025-01-11 19:10:12 -0700

    Working on figuring out docling. Can I load text without converting to markdown?

--- test_pdf_conversion.py
+++ test_pdf_conversion.py
@@ -1,22 +1,34 @@
-from docling.document_converter import DocumentConverter
+from docling.document_converter import DocumentConverter, ConversionResult
+from docling_core.types import DoclingDocument
 import pathlib
 import pymupdf4llm
 
 # Test docing
 source = "documents/Realism and the Aim of Science -- Karl Popper -- 2017.pdf"  # document per local path or URL
-# converter = DocumentConverter()
-# print("Converting document using Docling...")
-# result = converter.convert(source)
+converter: DocumentConverter = DocumentConverter()
+print("Converting document using Docling...")
+result: ConversionResult = converter.convert(source)
+doc: DoclingDocument = result.document
 # print("Conversion complete.")
-# markdown = result.document.export_to_markdown()
-# # Save file
-# output_path = pathlib.Path("test_docling.md")
-# output_path.write_text(markdown, encoding="utf-8")
+# Save file
+output_path = pathlib.Path("test_docling.md")
+markdown = doc.export_to_markdown()
+output_path.write_text(markdown, encoding="utf-8")
+output_path = pathlib.Path("test_docling.json")
+doc.save_as_json(output_path)
+# Loop over text in the document
+for text in doc.texts:
+    print(text.text)
+    print("\n\n")
+
+new_doc = doc.load_from_json(output_path)
+pass
+
 
 # Now try with PyMuPDF4LLM
-print("Converting document using PyMuPDF4LLM...")
-markdown = pymupdf4llm.to_markdown(source)
-# Save file
-output_path = pathlib.Path("test_pymupdf4llm.md")
-output_path.write_text(markdown, encoding="utf-8")
+# print("Converting document using PyMuPDF4LLM...")
+# markdown = pymupdf4llm.to_markdown(source)
+# # Save file
+# output_path = pathlib.Path("test_pymupdf4llm.md")
+# output_path.write_text(markdown, encoding="utf-8")
 
